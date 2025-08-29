ad2f9ab1a7620b6effa8bbba12af0407c1893e9d
jasonko <jason.ko@futurevaultinc.com>
2025-08-28 21:02:15 -0400

    adding WIP parallel/task examples with controlled limit using SemaphoreSlim

---
+++ Concurrency/ParallelExample/ParallelControlledLimit.cs
@@ -0,0 +1,17 @@
+using CSharpAppPlayground.UIClasses;
+
+namespace CSharpAppPlayground.Concurrency.ParallelExample
+{
+    /// <summary>
+    /// This example uses Parallel to demonstrate parallel processing in C#.
+    /// </summary>
+    public class ParallelControlledLimit : UIFormRichTextBoxHelper
+    {
+        // private Form f;
+        public async void Run()
+        {
+           // TODO
+        }
+
+    }
+}
