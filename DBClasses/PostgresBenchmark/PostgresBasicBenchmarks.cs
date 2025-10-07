ee6698025d2a557905242efed65a30573d3f584b
jkohome <jasonko.develop@gmail.com>
2025-10-07 13:33:02 -0400

    WIP: adding more benchmarks placeholders

---
+++ DBClasses/PostgresBenchmark/PostgresBasicBenchmarks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAppPlayground.DBClasses.PostgresBenchmark
+{
+    public class PostgresBasicBenchmarks
+    {
+    }
+}
