e7f250435497ae587a2d9766cabb5339d4484d60
jkohome <jasonko.develop@gmail.com>
2025-10-10 17:35:39 -0400

    DataGen for testing DONE: ran more tests added summaries and added condition in GenerateData() function to use different option based on count. note: any number < 1000 single thread is faster probably because of the overhead from Parallel and ConcurrentBag

--- Classes/DataGen/Generators/GenerateVidsSQL.cs
+++ Classes/DataGen/Generators/GenerateVidsSQL.cs
@@ -1,5 +1,4 @@
 ﻿using CSharpAppPlayground.DBClasses.Data.SQLbenchmark;
-using Google.Protobuf.WellKnownTypes;
 using MethodTimer;
 using System.Collections.Concurrent;
 
@@ -19,8 +18,10 @@ namespace CSharpAppPlayground.Classes.DataGen.Generators
 
         public List<VidsSQL> GenerateData(int count)
         {
-            //return SingleThread(count);
-            return ParallelThread(count);
+            if(count <= 1000)
+                return SingleThread(count); // less than or equal to 1000, use single thread its faster
+            else
+                return ParallelThread(count);
         }
 
         [Time("GenerateVidsSQL.SingleThread({count})")]
@@ -86,11 +87,11 @@ namespace CSharpAppPlayground.Classes.DataGen.Generators
         // performance test method
         public void TestPerformance()
         {
-            //_ = GenerateData(1000); // Generate 1000 records for testing
-            //_ = GenerateData(5000); // Generate 5000 records for testing
-            //_ = GenerateData(10000); // Generate 10000 records for testing
-            //_ = GenerateData(100000); // Generate 100000 records for testing
-            //_ = GenerateData(1000000); // Generate 1 million records for testing
+            _ = GenerateData(1000); // Generate 1000 records for testing
+            _ = GenerateData(5000); // Generate 5000 records for testing
+            _ = GenerateData(10000); // Generate 10000 records for testing
+            _ = GenerateData(100000); // Generate 100000 records for testing
+            _ = GenerateData(1000000); // Generate 1 million records for testing
             _ = GenerateData(10000000); // Generate 10 million records for testing
         }
     }
@@ -110,3 +111,30 @@ namespace CSharpAppPlayground.Classes.DataGen.Generators
 // Method 'ParallelThread' executed in 00:00:06.7269346 ms, GenerateVidsSQL.ParallelThread(10000000)
 // Parition: 10000
 // Method 'ParallelThread' executed in 00:00:06.8805132 ms, GenerateVidsSQL.ParallelThread(10000000)
+// Removing Parition logic, added 2 seconds to average with partition
+// Method 'ParallelThread' executed in 00:00:08.7369940 ms, GenerateVidsSQL.ParallelThread(10000000)
+
+// FULL List
+//
+// Parition: 10000
+//Method 'ParallelThread' executed in 00:00:00.0172685 ms, GenerateVidsSQL.ParallelThread(1000)
+//Method 'ParallelThread' executed in 00:00:00.0089994 ms, GenerateVidsSQL.ParallelThread(5000)
+//Method 'ParallelThread' executed in 00:00:00.0230335 ms, GenerateVidsSQL.ParallelThread(10000)
+//Method 'ParallelThread' executed in 00:00:00.1348448 ms, GenerateVidsSQL.ParallelThread(100000)
+//Method 'ParallelThread' executed in 00:00:00.9340628 ms, GenerateVidsSQL.ParallelThread(1000000)
+//Method 'ParallelThread' executed in 00:00:06.0004151 ms, GenerateVidsSQL.ParallelThread(10000000)
+//
+//Method 'SingleThread' executed in 00:00:00.0040979 ms, GenerateVidsSQL.SingleThread(1000)
+//Method 'SingleThread' executed in 00:00:00.0087623 ms, GenerateVidsSQL.SingleThread(5000)
+//Method 'SingleThread' executed in 00:00:00.0223416 ms, GenerateVidsSQL.SingleThread(10000)
+//Method 'SingleThread' executed in 00:00:00.1881593 ms, GenerateVidsSQL.SingleThread(100000)
+//Method 'SingleThread' executed in 00:00:01.2953029 ms, GenerateVidsSQL.SingleThread(1000000)
+//Method 'SingleThread' executed in 00:00:10.8450782 ms, GenerateVidsSQL.SingleThread(10000000)
+//
+// Parition: 2000
+//Method 'ParallelThread' executed in 00:00:00.0211852 ms, GenerateVidsSQL.ParallelThread(1000)
+//Method 'ParallelThread' executed in 00:00:00.0058897 ms, GenerateVidsSQL.ParallelThread(5000)
+//Method 'ParallelThread' executed in 00:00:00.0147914 ms, GenerateVidsSQL.ParallelThread(10000)
+//Method 'ParallelThread' executed in 00:00:00.1512057 ms, GenerateVidsSQL.ParallelThread(100000)
+//Method 'ParallelThread' executed in 00:00:00.8186177 ms, GenerateVidsSQL.ParallelThread(1000000)
+//Method 'ParallelThread' executed in 00:00:06.4458419 ms, GenerateVidsSQL.ParallelThread(10000000)
