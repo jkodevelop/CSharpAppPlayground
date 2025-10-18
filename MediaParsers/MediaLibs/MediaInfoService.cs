0fbc8f6f58dcf31eed52352c90cfc1580d3bf4e7
jkohome <jasonko.develop@gmail.com>
2025-10-18 09:51:53 -0400

    added MediaLib

---
+++ MediaParsers/MediaLibs/MediaInfoService.cs
@@ -0,0 +1,33 @@
+﻿using MediaInfo;
+
+namespace CSharpAppPlayground.MediaParsers.MediaLibs
+{
+    public class MediaInfoService
+    {
+        protected MediaInfo.MediaInfoWrapper mediaInf;
+
+        public MediaInfoService(string filePath)
+        {
+            // supports ILogger
+            // example: mediaInf = new MediaInfo.MediaInfoWrapper(filePath, logger);
+            
+            mediaInf = new MediaInfo.MediaInfoWrapper(filePath);
+            if (!mediaInf.Success)
+            {
+                throw new Exception($"Failed to get MediaInfo {filePath}");
+            }
+        }
+
+        public int GetDuration()
+        {
+            return mediaInf.Duration;
+        }
+
+        public (int width, int height) GetMediaDimensions()
+        {
+            int w = mediaInf.Width;
+            int h = mediaInf.Height;
+            return (w, h);
+        }
+    }
+}
