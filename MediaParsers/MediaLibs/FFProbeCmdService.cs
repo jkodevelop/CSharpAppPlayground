a66655f8fd6b52a948be7376d255c800e1001e83
jkohome <jasonko.develop@gmail.com>
2025-10-25 00:25:03 -0400

    WIP fixing FFProbe processing locked to video issue

--- MediaParsers/MediaLibs/FFProbeCmdService.cs
+++ MediaParsers/MediaLibs/FFProbeCmdService.cs
@@ -1,6 +1,38 @@
 ﻿using System.Diagnostics;
 
 // REQUIRED: ffmpeg and ffprobe must be installed and accessible in the system PATH for this code to work.
+// command line is more sensitive to quotes and media types than using ffmpeg libraries directly.
+
+/*
+ffprobe -v error -show_entries stream=width,height,duration -of default ".\file.opus"
+ffprobe -v error -show_entries stream=width,height,duration -of default ".\file.mp4"
+
+example output for .mp4 file:
+[STREAM]
+width=640
+height=360
+duration=8505.363533
+[/STREAM]
+[STREAM]
+duration=8505.422948
+[/STREAM]
+
+example output for .opus file:
+[STREAM]
+duration=10341.207500
+[/STREAM]
+
+ffprobe -v error -select_streams v:0 -show_entries stream=duration,width,height -of  default=noprint_wrappers=1 ".\file.mp4"
+ffprobe -v error -show_entries stream=duration,width,height -of  default=noprint_wrappers=1 ".\file.opus"
+
+example output for .mp4 file:
+width=640
+height=360
+duration=8505.363533
+
+example output for .opus file:
+duration=10341.207500
+ */
 
 namespace CSharpAppPlayground.MediaParsers.MediaLibs
 {
@@ -33,9 +65,6 @@ namespace CSharpAppPlayground.MediaParsers.MediaLibs
 
         public (int width, int height) GetMediaDimensions(string filePath)
         {
-            int w = -1;
-            int h = -1;
-
             //TODO: validate filePath
             var psi = new ProcessStartInfo
             {
@@ -50,23 +79,30 @@ namespace CSharpAppPlayground.MediaParsers.MediaLibs
             string output = proc!.StandardOutput.ReadToEnd();
             proc.WaitForExit();
 
-            if (double.TryParse(output.Trim(), System.Globalization.CultureInfo.InvariantCulture, out double seconds))
-                return (-1, -1);
+            var lines = output.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return (w, h);
+            if (lines.Length >= 2 &&
+                int.TryParse(lines[0].Trim(), out int w) &&
+                int.TryParse(lines[1].Trim(), out int h))
+            {
+                return (w, h);
+            }
+
+            return (-1, -1);
         }
 
+        // FIX for audio files that do not have width/height
         public (int width, int height, int duration) GetVideoProperties(string filePath)
         {
             int w = -1;
             int h = -1;
             int duration = -1;
 
-            //TODO: validate filePath
+            // TODO: validate filePath
             var psi = new ProcessStartInfo
             {
                 FileName = "ffprobe",
-                Arguments = $"-v error -select_streams v:0 -show_entries stream=width,height,duration -of default=noprint_wrappers=1:nokey=1 \"{filePath}\"",
+                Arguments = $"-v error -show_entries stream=width,height,duration -of default=noprint_wrappers=1 \"{filePath}\"",
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
                 CreateNoWindow = true
@@ -76,10 +112,20 @@ namespace CSharpAppPlayground.MediaParsers.MediaLibs
             string output = proc!.StandardOutput.ReadToEnd();
             proc.WaitForExit();
 
-            if (double.TryParse(output.Trim(), System.Globalization.CultureInfo.InvariantCulture, out double seconds))
-                return (-1, -1, -1);
+            // FFProbe output will be three lines: width\nheight\nduration
+            var lines = output.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return (w, h, duration); // Return invalid dimensions if unsuccessful
+            if (lines.Length >= 3)
+            {
+                if (int.TryParse(lines[0].Trim(), out int parsedW))
+                    w = parsedW;
+                if (int.TryParse(lines[1].Trim(), out int parsedH))
+                    h = parsedH;
+                if (double.TryParse(lines[2].Trim(), System.Globalization.CultureInfo.InvariantCulture, out double seconds))
+                    duration = (int)seconds;
+            }
+
+            return (w, h, duration);
         }
     }
 }
