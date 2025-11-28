929cb94d6318d0705b73e5baa8e20c6993a28ae8
jkohome <jasonko.develop@gmail.com>
2025-11-28 18:18:56 -0500

    <a> has modified_date attribute, so adding it to link object

--- FilesFolders/Files/BookmarkLibrary.cs
+++ FilesFolders/Files/BookmarkLibrary.cs
@@ -7,8 +7,9 @@ namespace CSharpAppPlayground.FilesFolders.Files
         public string? Name { get; set; }
         public string Url { get; set; } = "";
         public string AddDateUnixTimeSeconds { get; set; }
-        public DateTimeOffset? AddDate { get; set; } // use string instead since its all UNIXTIMESECONDS
-        // public DateTimeOffset ModifiedDate { get; set; } // exported bookmarks don't have modified date for links
+        public DateTimeOffset? AddDate { get; set; } // add_date - use string instead since its all UNIXTIMESECONDS
+        public string ModifiedDateUnixTimeSeconds { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; } // last_modified
     }
 
     public class FolderBookmark
